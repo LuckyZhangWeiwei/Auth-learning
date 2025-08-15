c3e92def37ee0bf1e4c87dd189078215c5e4994c
LuckyZhangWeiwei <zhangzhang_10@hotmail>
2025-08-15 12:51:33 +0800

    init

---
+++ IdentityManagement/Program.cs
@@ -0,0 +1,143 @@
+using System.Security.Claims;
+using IdentityManagement;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Identity;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder
+    .Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+    .AddCookie(CookieAuthenticationDefaults.AuthenticationScheme);
+builder.Services.AddAuthorization(b =>
+{
+    b.AddPolicy(
+        "manager",
+        pb =>
+        {
+            pb.RequireAuthenticatedUser()
+                .AddAuthenticationSchemes(CookieAuthenticationDefaults.AuthenticationScheme)
+                .RequireClaim("role", "manager");
+        }
+    );
+});
+builder.Services.AddSingleton<Database>();
+builder.Services.AddSingleton<IPasswordHasher<User>, PasswordHasher<User>>();
+builder.Services.AddDataProtection();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapGet("/", () => "hello world");
+app.MapGet("/protected", () => "sth super secret").RequireAuthorization("manager");
+
+app.MapGet(
+    "/register",
+    async (
+        string username,
+        string password,
+        IPasswordHasher<User> hasher,
+        Database db,
+        HttpContext ctx
+    ) =>
+    {
+        var user = new User() { Username = username };
+        user.PasswordHash = hasher.HashPassword(user, password);
+        await db.PutAsync(user);
+        await ctx.SignInAsync(
+            CookieAuthenticationDefaults.AuthenticationScheme,
+            UserHelper.Convert(user)
+        );
+        return user;
+    }
+);
+
+app.MapGet(
+    "/login",
+    async (
+        string username,
+        string password,
+        IPasswordHasher<User> hasher,
+        Database db,
+        HttpContext ctx
+    ) =>
+    {
+        var user = await db.GetUserAsync(username);
+
+        if (user == null)
+            return "username error";
+
+        var result = hasher.VerifyHashedPassword(user, user.PasswordHash, password);
+
+        if (result == PasswordVerificationResult.Failed)
+        {
+            return "bad credentials";
+        }
+        await ctx.SignInAsync(
+            CookieAuthenticationDefaults.AuthenticationScheme,
+            UserHelper.Convert(user)
+        );
+        return "loggin";
+    }
+);
+
+app.MapGet(
+    "/promote",
+    async (string username, Database db) =>
+    {
+        var user = await db.GetUserAsync(username);
+
+        if (user == null)
+            return "username error";
+
+        user.Claims.Add(new UserClaim { Type = "role", Value = "manager" });
+
+        if (user == null)
+            return "username error";
+        await db.PutAsync(user);
+
+        return "promoted";
+    }
+);
+
+app.MapGet(
+    "/start-password-reset",
+    async (string username, Database db) =>
+    {
+        var user = await db.GetUserAsync(username);
+
+        if (user == null)
+            return "username error";
+
+        user.Claims.Add(new UserClaim { Type = "role", Value = "manager" });
+
+        if (user == null)
+            return "username error";
+        await db.PutAsync(user);
+
+        return "promoted";
+    }
+);
+
+app.Run();
+
+public class UserHelper
+{
+    public static ClaimsPrincipal Convert(User user)
+    {
+        var claims = new List<Claim>() { new Claim("username", user.Username) };
+
+        claims.AddRange(user.Claims.Select(x => new Claim(x.Type, x.Value)));
+
+        var identity = new ClaimsIdentity(
+            claims,
+            CookieAuthenticationDefaults.AuthenticationScheme
+        );
+
+        return new ClaimsPrincipal(identity);
+    }
+}
--- Roles/obj/Debug/net9.0/Roles.AssemblyInfo.cs
+++ Roles/obj/Debug/net9.0/Roles.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Roles")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+25242844b5f8263735e4cc11ef8da2141b257ce3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d507d4a4ee49923babfd87d3d4e536b696e2e601")]
 [assembly: System.Reflection.AssemblyProductAttribute("Roles")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Roles")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
